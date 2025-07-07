9761fbdcb5e2db960be5f9578018e7e55bf97b88
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-07-07 20:34:16 +0300

    add:Implement more appwrite logic

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
     endpoint:process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     projectId:process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
     platform:"com.codebasejournal.foodordering",
+    databaseId:'686ae9800037770be80f',
+    userCollectionId:"686ae9af000ac1a388a6"
 }
