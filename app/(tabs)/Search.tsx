2394dde610afc49135d6e5909d39e3361314c2ac
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-07-08 19:54:52 +0300

    add:Initial UI for seeding the database as well as the bucket

--- app/(tabs)/Search.tsx
+++ app/(tabs)/Search.tsx
@@ -1,10 +1,13 @@
-import {View, Text} from 'react-native'
+import {View, Text, Button} from 'react-native'
+import {SafeAreaView} from "react-native-safe-area-context";
+import seed from "@/lib/seed";
 
 const Search = () => {
     return (
-        <View>
+        <SafeAreaView>
             <Text>Search</Text>
-        </View>
+            <Button title={'seed'} onPress={()=>seed().catch((error)=> console.log("Failed to seed the database",error.message))}/>
+        </SafeAreaView>
     )
 }
 export default Search
